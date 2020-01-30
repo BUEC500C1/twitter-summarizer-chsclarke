0c9f23a9c93144ebccdc47412f371ae2d3c04af6
Chase Clarke <chaseclarke@Chase-2.local>
2020-01-30 18:52:30 -0500

    code cleanup

--- main.py
+++ main.py
@@ -11,65 +11,68 @@ twitter = API.Twitter('auth/twitterAuth.json')
 """get last tweet given username"""
 @app.route('/get_tweet')
 def get_last_tweet():
-
-    if (request.args.get('username')):
-        username = request.args.get('username')
-    else:
-        return "{\"ERROR\" : \"you must enter a username\"}"
-        
     result = {}
     apiCall = None
     status = None
 
-    status = twitter.get_user_timeline(username, 1)
-    
-    try:
-        imgURL = twitter.get_image(status[0])
-        apiCall = google.get_image_description(imgURL)
-        result["img"] = [apiCall[0], apiCall[1], apiCall[2]]
-    except:
-        apiCall = False
+    #if user provided a username
+    if (request.args.get('username')):
+        username = request.args.get('username')
+        status = twitter.get_user_timeline(username, 1)
+
+        #check if there is an image in tweet
+        try:
+            imgURL = twitter.get_image(status[0])
+            apiCall = google.get_image_description(imgURL)
+            result["img"] = [apiCall[0], apiCall[1], apiCall[2]]
+        except:
+            apiCall = False
         
-    while (status is None or apiCall is None):
-        time.sleep(.1)
+        #wait for api data to return
+        while (status is None or apiCall is None):
+            time.sleep(.1)
 
-    result["tweet"] = status[0].text
+        result["tweet"] = status[0].text
 
-    return result
+        return result
+    else:
+        return "{\"ERROR\" : \"you must enter a username\"}"
 
 
 """get profile info given username"""
 @app.route('/get_profile')
 def get_user_profile():
-
-    if (request.args.get('username')):
-        username = request.args.get('username')
-    else:
-        return "{\"ERROR\" : \"you must enter a username\"}"
-
     result = {}
     imgDescription = None
     status = None
 
-    status = twitter.get_user_timeline(username, 1)
-    profile = twitter.get_user_profile(status[0])
-    imgDescription = google.get_image_description(profile["profile_image_url_https"])
-    print(profile["profile_background_image_url_https"])
+    #if user provided a username
+    if (request.args.get('username')):
+        username = request.args.get('username')
+        status = twitter.get_user_timeline(username, 1)
+        profile = twitter.get_user_profile(status[0])
+        imgDescription = google.get_image_description(profile["profile_image_url_https"])
+        print(profile["profile_background_image_url_https"])
 
-    while (status is None or imgDescription is None):
-        time.sleep(.1)
+        #wait for api data to return
+        while (status is None or imgDescription is None):
+            time.sleep(.1)
 
-    result["created_at"] = profile["created_at"]
-    result["description"] = profile["description"]
-    result["urls"] = profile["entities"]["url"]["urls"]
-    result["followers_count"] = profile["followers_count"]
-    result["id"] = profile["id"]
-    result["location"] = profile["location"]
-    result["name"] = profile["name"]
-    result["screen_name"] = profile["screen_name"]
-    result["img"] = [imgDescription[0], imgDescription[1], imgDescription[2]]
+        result["created_at"] = profile["created_at"]
+        result["description"] = profile["description"]
+        result["urls"] = profile["entities"]["url"]["urls"]
+        result["followers_count"] = profile["followers_count"]
+        result["id"] = profile["id"]
+        result["location"] = profile["location"]
+        result["name"] = profile["name"]
+        result["screen_name"] = profile["screen_name"]
+        result["img"] = [imgDescription[0], imgDescription[1], imgDescription[2]]
 
-    return result
+        return result
+    else:
+        return "{\"ERROR\" : \"you must enter a username\"}"
+
+
 
 
 if __name__ == '__main__':
