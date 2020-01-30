6f1aef1bb191de9f8046f592eee9e2c7f1da876f
Chase Clarke <chaseclarke@Chase-2.local>
2020-01-30 18:44:23 -0500

    update gitignore

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import API
 import json
 
+
 twitter = API.Twitter('auth/twitterAuth.json')
 status = twitter.get_user_timeline('realdonaldtrump', 1)
 
@@ -8,4 +9,5 @@ profile = twitter.get_user_profile(status[0])
 
 # print(json.dumps(status[0].__json, indent=4, sort_keys=True))
 
+# print(json.dumps(profile, indent=4, sort_keys=True))
 print(json.dumps(profile, indent=4, sort_keys=True))
